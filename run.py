05a8f9899481786c77f6b103c12ffe4c19494f32
bubus2 <bubus258@gmail.com>
2021-06-21 02:19:29 +0200

    Moved input logic to encode.py

--- run.py
+++ run.py
@@ -1,12 +1,4 @@
-import src.encode
+from src.encode import get_input
 
 if __name__ == "__main__":
-    text = ""
-    while text == "":
-        text = input("Input text: ")
-        if text == "":
-            print("Text shouldn't be empty! Try Again!")
-
-    src.encode.core(text)
-    print("Press Enter to exit the program:")
-    input()
+    get_input()
